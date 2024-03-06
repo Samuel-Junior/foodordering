2f80ba233415f06830974a15a4e5c989760375f2
Samuel-Jr Ndolou <samuel-jr@MBPdeSamuelJr.lan>
2024-03-07 00:03:30 +0100

    add of paths aliasses

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,14 +1,18 @@
 import { StyleSheet, Text, View, Image } from 'react-native';
 import  Colors from '../constants/Colors';
+import  {Product} from '../types';
 
+export const defaultImagePizza = "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png"
 
+type ProductListItemProps = {
+    product : Product
+}
 
-
-const ProductListItem = ({product})=>{
+const ProductListItem = ({product} : ProductListItemProps)=>{
    
   return(
     <View style={styles.container}>
-      <Image source={{ uri: product.image}} style={styles.image}/>
+      <Image source={{ uri: product.image || defaultImagePizza}} style={styles.image}/>
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>{product.price}</Text>
     </View>
